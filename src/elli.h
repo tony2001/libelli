1a4deba594ed91e53b48aa0e9290ce53eb171be7
Antony Dovgal <tony@daylessday.org>
2013-11-22 18:38:52 +0400

    add comments

--- src/elli.h
+++ src/elli.h
@@ -24,6 +24,18 @@ elli_ctx_t *elli_ctx_create();
 char *elli_ctx_last_error(elli_ctx_t *ctx);
 void elli_ctx_free(elli_ctx_t *ctx);
 
+/*
+ to generate a key file use this:
+   openssl ecparam -name secp112r1 -genkey -out key.pem
+ you can use any curve type instead of secp112r1, see full list with this command:
+   openssl ecparam -list_curves
+*/
+
+/*
+ keys is specified in HEX form, use this command:
+   openssl ec -in key.pem -text -noout
+ and remove the colons from the output.
+ */
 verbum_t *elli_encrypt(elli_ctx_t *ctx, char *key, unsigned char *data, size_t length);
 unsigned char *elli_decrypt(elli_ctx_t *ctx, char *key, verbum_t *encrypted, size_t *length);
 
